e4386faadfcbcadf2e1a61f7f35508b8f86712d5
snowyru <97807830+snowyru@users.noreply.github.com>
2023-01-25 21:48:34 +0200

    transplanting navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
       </nav>
       <head />
       <body>{children}</body>
+      <footer>
+        <h3 className="pt-3">Footer</h3>
+      </footer>
     </html>
   )
 }
