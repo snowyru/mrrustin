ecae5b4562bf7b7818ff44ce38c180520c255b4b
snowyru <97807830+snowyru@users.noreply.github.com>
2023-02-08 20:58:47 +0200

    updated color theme and home content

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
                         <span className="font-semibold">Mr Rustin</span>
                     </h2>
                     <p className="text-gray-600 dark:text-gray-400 mb-16">
-                        Hi I'm Shahied, a student at CPUT. Currently, a full stack web developer and IT specialist. Nice to meet you 🤝
+                        Hi I'm Shahied, electronic⚡ / computer💻 engineer student at <a className="text-blue-700  dark:text-blue-300" target="_blank" href="https://www.cput.ac.za" >CPUT🎓</a>. Currently, a full stack web developer and IT specialist. Nice to meet you 🤝
                     </p>
                     </div>
                     <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto pt-5">
@@ -40,7 +40,7 @@ export default function HomePage() {
                         "Shahied, A thriving web developer and accomplished IT manager, possesses a passion for technology and a dedication to helping others. Shahied brings a wealth of knowledge and expertise to the table, delivering innovative solutions that drive success for both individuals and organizations alike. Whether working with clients or leading a team, Shahied approaches every task with professionalism, determination, and a drive to succeed." - ChatGPT
                     </p>
                 <h3 className="font-bold decoration-lime-500 underline decoration-2 underline-offset-4 text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
-                    Featured Posts
+                    Featured Projects
                 </h3>
                 <div className="flex gap-6 flex-col md:flex-row">
 
@@ -69,35 +69,12 @@ export default function HomePage() {
                 </Link>
 
                 <h3 className="font-bold decoration-lime-500 underline decoration-2 underline-offset-4 text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
-                    Learn React & Next.js
+                    My Socials
                 </h3>
                 <p className="text-gray-600 dark:text-gray-400 mb-4">
-                    Build and deploy a modern SaaS application using the most popular
-                    open-source software. This course is 12 hours long and is completely
-                    live streamed.
+                    Check out my YouTube Channel ▶️
                 </p>
-                <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.youtube.com/playlist?list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1"
-                    className="flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
-                >
-                    Watch all videos
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="h-6 w-6 ml-1"
-                    >
-                    <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
-                    />
-                    </svg>
-                </a>
+                <iframe width="686" height="386" src="https://www.youtube.com/embed/bfresXpwGw8" title="Noordhoek beach" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </div>
         </Suspense>
     );
