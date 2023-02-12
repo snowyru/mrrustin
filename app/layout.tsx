8bb228c21ebaf98d829df02b5400464b32e70580
snowyru <97807830+snowyru@users.noreply.github.com>
2023-02-12 18:49:21 +0200

    clean up unused motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,26 +11,10 @@ export default function RootLayout({
   children: React.ReactNode
 }, dark: boolean) {
 
-  const { resolvedTheme, setTheme } = useTheme();
-  setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
-
-  const darkTransition = {
-    darkMode: {
-      scale:1
-    },
-    lightMode: {
-      scale:1
-    }
-  }
-
   return (
     <html>
       <body>
         <ThemeProvider attribute='class'>
-          <motion.div 
-          variants={darkTransition}
-          initial={'lightMode'}
-          animate={'darkMode'} >
             <main>
               <nav>
                 <Navbar/>
@@ -40,7 +24,6 @@ export default function RootLayout({
                 <h3 className=''>~end~</h3>
               </footer>
             </main>
-          </motion.div>
         </ThemeProvider>
       </body>
     </html>
